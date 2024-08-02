fc32b2b27801eb11f44099cb9bd7df1ca0c43e01
Seo <seo@dayoff.co.kr>
2024-08-02 17:17:44 +0900

    added about > our services

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,18 +1,12 @@
-import { BackgroundGradientAnimation } from "@/components/ui/background-gradient-animation";
 import { ABOUT } from "@/constants/about";
+import OurServices from "@/screens/about/OurServices";
 
 const Page = () => {
   return (
     <div className={`w-full flex flex-col gap-y-20 pt-10 pb-[150px]`}>
       <section className={`w-full flex flex-col rounded-lg overflow-hidden`}>
-        <BackgroundGradientAnimation>
-          <div
-            className={`relative flex flex-col justify-end gap-y-5 bg-white/50 w-full h-[50vh] max-h-[500px] z-10 p-10 backdrop-blur-sm`}
-          >
-            <h1 className={`text-5xl font-bold`}>{ABOUT.introduction.title}</h1>
-            <p className={"text-xl"}>{ABOUT.introduction.description}</p>
-          </div>
-        </BackgroundGradientAnimation>
+        <h1 className={`text-5xl font-bold`}>{ABOUT.introduction.title}</h1>
+        <p className={"text-xl"}>{ABOUT.introduction.description}</p>
       </section>
 
       <section className={`w-full flex flex-col`}>
@@ -50,21 +44,7 @@ const Page = () => {
         </ul>
       </section>
 
-      <section className={`w-full flex flex-col`}>
-        <h2 className={"subtitle"}>{ABOUT.ourTarotReadingServices.title}</h2>
-        <p>{ABOUT.ourTarotReadingServices.description}</p>
-
-        <ul>
-          {ABOUT.ourTarotReadingServices.list!.map((item, index) => {
-            return (
-              <li key={index}>
-                <h3 className={`font-semibold`}>{item.subtitle}</h3>
-                <p>{item.description}</p>
-              </li>
-            );
-          })}
-        </ul>
-      </section>
+      <OurServices />
 
       <section className={`w-full flex flex-col`}>
         <h2 className={"subtitle"}>{ABOUT.meetTheDeveloper.title}</h2>
---
+++ screens/about/OurServices.tsx
@@ -0,0 +1,33 @@
+import { Button } from "@/components/ui/button";
+import { ABOUT } from "@/constants/about";
+
+const OurServices = () => {
+  return (
+    <section className={`w-full flex flex-col gap-y-5 md:gap-y-10 px-5`}>
+      <h2 className={"subtitle"}>{ABOUT.ourTarotReadingServices.title}</h2>
+
+      <div className={`w-full flex flex-col gap-y-5`}>
+        <p>{ABOUT.ourTarotReadingServices.description}</p>
+
+        <ul className={`flex flex-col md:flex-row items-start gap-5`}>
+          {ABOUT.ourTarotReadingServices.list!.map((item, index) => {
+            return (
+              <li
+                key={index}
+                className={`h-[250px] w-full flex flex-col justify-between flex-1 shadow-card-default rounded-lg p-5 hover:translate-y-[-5px] gap-y-5 hover:cursor-pointer`}
+              >
+                <div className={`flex flex-col gapy-y-3`}>
+                  <h3 className={`font-semibold text-xl`}>{item.subtitle}</h3>
+                  <p>{item.description}</p>
+                </div>
+                <Button className={`self-end`}>See more</Button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default OurServices;
